5440662075ed7cfab1bd5f7cc67cda6e514c0fc7
oushesh <oushesh@gmail.com>
2026-03-12 11:53:11 +0100

    added VideoAnalyzer Rust

---
+++ VideoAnalyzer/memory/readme.md
@@ -0,0 +1,12 @@
+## Actix vs Axum
+   
+    Axum: The default choice 
+    Axum built behind the team of tokio: (Rust standard async runtime)
+    Pro: Ergonmomics, Easy to use and less macros
+    
+
+    Actix: High Performance but requires high level of programming skills in Rust for you to use that.
+
+## How do we transform the function or code into API?
+   Take the function --> JSONIFY IT FOR another function
+   into Rust then add the nitty gritt of the apps.
---
+++ VideoAnalyzer/memory/tests/api_tests.rs
@@ -0,0 +1,53 @@
+//2 ways to hand tests. inside the main.rs as "unit Test"
+// or separate folder --> useful when app and code grows
+
+//This tells Rust to only Compile this block when you r
+//a cargo test
+
+use axum::{body::Body, http::{Request, StatusCode}};
+use tower::ServiceExt;
+// Import from your library crate
+use memory::{create_app};
+#[tokio::test]
+async fn test_health_check() {
+    let app = create_app(); // This now has a concrete type the compiler can infer
+
+    let response = app
+        .oneshot(
+            Request::builder()
+                .uri("/health")
+                .body(Body::empty())
+                .unwrap()
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK);
+}
+
+//Similarly we can test
+#[tokio::test]
+async fn test_calculate_api_logic() 
+{
+    let app = create_app();
+    let response = app.oneshot(
+        Request::builder()
+            .method("POST")
+            .uri("/calculate")
+            .header("Content-Type", "application/json")
+            .body(Body::from(json!(
+                {
+                            "query_sim": 0.5,
+                            "positives": [1.0],
+                            "negatives": [0.0],
+                            "alpha": 0.1,
+                            "beta": 0.05
+                }
+            ).to_string()))
+            .unwrap(),
+    ).await.unwrap();
+    //perform an assertion in the question
+    assert_eq!(response.status(), StatusCode::OK);
+    }
+}
+
